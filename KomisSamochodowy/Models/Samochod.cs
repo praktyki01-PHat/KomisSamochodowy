41008896f8ebef3332d06be314e06df30dc0b08f
losttheone <kacper.kubon24@gmail.com>
2023-11-21 08:31:00 +0100

    Dodaj pliki projektów.

---
+++ KomisSamochodowy/Models/Samochod.cs
@@ -0,0 +1,12 @@
+﻿namespace KomisSamochodowy.Models
+{
+    public class Samochod
+    {
+        public int Id { get; set; }
+        public string Kolor { get; set; }
+        public float PojemnoscSilnika { get; set; }
+        public float Przebieg { get; set; }
+        public string NumerVIN { get; set; }
+        public float Cena { get; set; }
+    }
+}
